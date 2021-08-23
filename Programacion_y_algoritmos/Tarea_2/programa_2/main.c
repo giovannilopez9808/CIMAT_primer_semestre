475989a0ec6e4b55afc7e1ca3515937b0cfdf56a
giovannilopez9808 <giovannilopez9808@gmail.com>
2021-08-22 22:35:24 -0500

    Change subsection to section

---
+++ Programacion_y_algoritmos/Tarea_2/programa_2/main.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+int main()
+{
+    int number;
+    char letter;
+    scanf("%d", &number);
+    scanf("%c", &letter);
+    printf("%d\n", number);
+    printf("%c\n", letter);
+    return 0;
+}
