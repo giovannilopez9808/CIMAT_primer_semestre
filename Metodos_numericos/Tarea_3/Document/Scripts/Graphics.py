60d2a876e10a5e66a864b42825e340508725ce34
giovannilopez9808 <giovannilopez9808@gmail.com>
2021-08-31 12:13:42 -0500

    Update

--- Metodos_numericos/Tarea_3/Document/Scripts/Graphics.py
+++ Metodos_numericos/Tarea_3/Document/Scripts/Graphics.py
@@ -3,23 +3,18 @@ import numpy as np
 
 
 def f(x: list):
-    return (x*np.cos(x)*np.sin(x))/(x-np.sin(x))
+    return (x*np.cos(x)-np.sin(x))/(x-np.sin(x))
 
 
 parameters = {
     "path graphics": "../Graphics/",
     "graphics name": "limit.eps",
 }
-x1 = np.linspace(0.01, 1, 100)
-x2 = np.linspace(-1, -0.01, 100)
-y1 = f(x1)
-y2 = f(x2)
+x = np.linspace(-1, 1, 100)
+y = f(x)
 plt.xlim(-1, 1)
-plt.ylim(-100, 100)
-plt.plot(x1, y1,
-         color="#480ca8",
-         lw=3)
-plt.plot(x2, y2,
+plt.ylim(-2.1, -1.9)
+plt.plot(x, y,
          color="#480ca8",
          lw=3)
 plt.grid(ls="--",
