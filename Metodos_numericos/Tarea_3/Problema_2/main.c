f0001a219ed07cd10eb3d0159a9d1fdaec498d99
giovannilopez9808 <giovannilopez9808@gmail.com>
2021-08-31 23:25:15 -0500

    Add documentation

--- Metodos_numericos/Tarea_3/Problema_2/main.c
+++ Metodos_numericos/Tarea_3/Problema_2/main.c
@@ -11,7 +11,7 @@ double function(double x)
      */
     double down = x - sin(x);
     double up = x * cos(x) - sin(x);
-    // Descomentar para obetner un límite que no existe y comentar la linea de  arriba
+    // Descomentar para obtener un límite que no existe y comentar la linea de  arriba
     // double up = x * cos(x) * sin(x);
     double fx = up / down;
     return fx;
@@ -47,21 +47,32 @@ double function_with_series(double x)
 }
 double obtain_RD(double x, double y)
 {
+    /* 
+    Obtiene la diferencia relativa de dos datos, se da por verdadera la y
+     */
     double RD;
     RD = fabs(x - y) * 100 / fabs(y);
     return RD;
 }
 double calculate_limit(double x, double *diff)
 {
+    /* 
+    Evalua la funcion dada un valor de x. Se tomara su valor de signo contrario para evaluarlo por la izquieda o derecha. 
+     */
+    // Limite por la derecha
     double fi = function(x);
+    // Limite por la izquierda
     double fs = function(-x);
+    // Impresion de los valores en terminal
     printf("%lf\t%lf\t%lf\t%lf\n", -x, fs, x, fi);
     double limit;
     *diff = fabs(fs - fi);
+    // Calculo del limite si la diferencia es mayor a 10^-6
     if (*diff < 1e-6)
     {
         limit = (fs + fi) / 2;
     }
+    // Si no, se da un valor arbitrario
     else
     {
         limit = 1;
@@ -70,40 +81,56 @@ double calculate_limit(double x, double *diff)
 }
 void find_limit()
 {
+    // Inicializacion de la diferencia y el limite
     double diff = 1;
     double limit = 1;
+    // Inicio del intervalo [-x,x]
     double x = 1;
+    // Impresion de los headers
     printf("x\t\tLi\t\tx\t\tLs\n");
     for (int i = 0; i < 12; i++)
     {
         limit = calculate_limit(x, &diff);
+        // Reduccion del intervalo
         x = x / 2;
     }
     printf("\nEl limite cuando f(x) tiende a cero ");
+    // Si el limite no existe se imprimira que no existe
     if (diff > 1e-6)
     {
         printf("no existe\n");
     }
+    // Si existe entonces se imprimesu valor
     else
     {
         printf("es %lf\n", limit);
     }
 }
+void print_RD(double fx, double fx_rounded, double fx_series)
+{
+    // Calculo de las diferencias relativas
+    double RD_fx_rounded = obtain_RD(fx_rounded, fx);
+    double RD_fx_series = obtain_RD(fx_series, fx);
+    print_lines();
+    // Impresion de
+    printf("La diferencia relativa de f(x)\n");
+    printf("\tCon redondeo a 4 decimales es: %.4lf\n", RD_fx_rounded);
+    printf("\tCon series es: %.4lf\n", RD_fx_series);
+}
 int main()
 {
     print_lines();
+    // Busqueda del limite
     find_limit();
     double x = 0.1;
     print_lines();
     printf("El valor de f(x)\n");
-    double fx_approx = function_with_series(x);
-    double fx_4 = function_four_digits(x);
+    // Evaluacion de las funciones redondeando a cuatro digitos y por serie
+    double fx_series = function_with_series(x);
+    double fx_rounded = function_four_digits(x);
+    // Valor exacto de la funcion
     double fx = function(x);
-    double RD_fx_4 = obtain_RD(fx_4, fx);
-    double RD_fx_approx = obtain_RD(fx_approx, fx);
-    print_lines();
-    printf("La diferencia relativa de f(x)\n");
-    printf("\tCon redondeo a 4 decimales es: %.4lf\n", RD_fx_4);
-    printf("\tCon series es: %.4lf\n", RD_fx_approx);
+    // Calculo de las diferencias relativas
+    print_RD(fx, fx_rounded, fx_series);
     return 0;
 }
