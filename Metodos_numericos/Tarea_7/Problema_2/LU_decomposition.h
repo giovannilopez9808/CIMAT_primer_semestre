8c8304e6d1c92787999b9fcf26bfe679918d5a11
giovannilopez9808 <giovannilopez9808@gmail.com>
2021-09-13 14:37:45 -0500

    Add problem 2 and documentation

---
+++ Metodos_numericos/Tarea_7/Problema_2/LU_decomposition.h
@@ -0,0 +1,74 @@
+void Fill_initial_values_U_matrix(double *U, int dimension_matrix[])
+{
+    /* 
+    Inicializa la matrix U con unos en la diagonal
+    inputs:
+    + U: puntero del arreglo de la matriz U
+    + dimension_matrix: arreglo de dos dimensiones que contiene el tamaño de la matriz
+     */
+    double *U_ii;
+    for (int i = 0; i < dimension_matrix[0]; i++)
+    {
+
+        U_ii = (U + i * dimension_matrix[0] + i);
+        *U_ii = 1;
+    }
+}
+void obtain_LU_crout(double *matrix, int dimension_matrix[], double **L, double **U)
+{
+    /* 
+    Aplica la factorizacion LU aplicando la version de Crout
+    inputs:
+    + matrix: puntero del arreglo que contiene lo datos de la matriz
+    + dimension_matrix: puntero del arreglo de dos dimensiones que contiene el tamaño de la matriz
+    + L: doble puntero que contendra la matriz L
+    + U: doble puntero que contendra la matriz U
+     */
+    *L = (double *)malloc((dimension_matrix[0]) * dimension_matrix[1] * sizeof(double));
+    *U = (double *)malloc((dimension_matrix[0]) * dimension_matrix[1] * sizeof(double));
+    // Apunteadores para guardar cada valor
+    double *L_ij, *U_ij;
+    double sum_ij, l_ii, l_ik, u_kj, matrix_ij;
+    // Inicializacion de la matriz U
+    Fill_initial_values_U_matrix(*U,
+                                 dimension_matrix);
+    for (int i = 0; i < dimension_matrix[0]; i++)
+    {
+        // Procedimiento para obtener los elementos de la matriz L
+        for (int j = i; j < dimension_matrix[1]; j++)
+        {
+            // Inicializacion de la suma
+            sum_ij = 0;
+            // Localizacion de la matriz L donde se calculara el elemento
+            L_ij = (*L + j * dimension_matrix[0] + i);
+            // Valor ij de la matriz
+            matrix_ij = *(matrix + i * dimension_matrix[0] + j);
+            for (int k = 0; k <= i - 1; k++)
+            {
+                l_ik = *(*L + j * dimension_matrix[0] + k);
+                u_kj = *(*U + k * dimension_matrix[0] + i);
+                sum_ij += l_ik * u_kj;
+            }
+            // Guardado del valor de L_ij
+            *L_ij = matrix_ij - sum_ij;
+        }
+        // Procedimiento para obtener el elemento ij de la matriz U
+        l_ii = *(*L + i * dimension_matrix[0] + i);
+        for (int j = i + 1; j < dimension_matrix[0]; j++)
+        {
+            // Inicializacion de la suma
+            sum_ij = 0;
+            // Localizacion de la matriz U donde se calculara el elemento
+            U_ij = (*U + i * dimension_matrix[0] + j);
+            matrix_ij = *(matrix + j * dimension_matrix[0] + i);
+            for (int k = 0; k < j - 1; k++)
+            {
+                l_ik = *(*L + i * dimension_matrix[0] + k);
+                u_kj = *(*U + k * dimension_matrix[0] + j);
+                sum_ij += l_ik * u_kj;
+            }
+            // Guardado del valor de L_ij
+            *U_ij = (matrix_ij - sum_ij) / l_ii;
+        }
+    }
+}
--- Metodos_numericos/Tarea_7/Problema_2/main.c
+++ Metodos_numericos/Tarea_7/Problema_2/main.c
@@ -1,38 +1,51 @@
 #include <stdio.h>
-#include <math.h>
 #include <stdlib.h>
 #include "functions.h"
 #include "read_files.h"
 #include "print_results.h"
-#include "Cholesky_decomposition.h"
+#include "LU_decomposition.h"
 #include "solution.h"
 int main(int argc, char *argv[])
 {
     (void)argc;
-    FILE *file_matrix, *file_L, *file_LT;
-    double *matrix, *L, *LT;
-    int dimension_matrix[2];
+    FILE *file_matrix, *file_results;
+    double *matrix, *results, *solutions, *L, *U, *L_solutions;
+    int dimension_matrix[2],
+        dimension_result[2];
     file_matrix = fopen(argv[1], "r");
-    file_L = fopen("L_test.txt", "w");
-    file_LT = fopen("LT_test.txt", "w");
+    file_results = fopen(argv[2], "r");
     valid_file(file_matrix);
+    valid_file(file_results);
     // Lectura de los datos de la matriz
     read_dimension(file_matrix,
                    dimension_matrix);
     read_matrix(file_matrix,
                 dimension_matrix,
                 &matrix);
-    obtain_Cholesky(matrix,
+    // Lectura de los datos del vector resultado
+    read_dimension(file_results,
+                   dimension_result);
+    read_matrix(file_results,
+                dimension_result,
+                &results);
+    obtain_LU_crout(matrix,
                     dimension_matrix,
                     &L,
-                    &LT);
-    write_matrix_on_file(file_L,
-                         L,
-                         dimension_matrix);
-    write_matrix_on_file(file_LT,
-                         LT,
-                         dimension_matrix);
+                    &U);
+    // Resuelve el sistema de ecuaciones
+    solve_triangular_inferior_matrix(L,
+                                     dimension_matrix,
+                                     results,
+                                     &L_solutions);
+    solve_triangular_superior_matrix(U,
+                                     dimension_matrix,
+                                     L_solutions,
+                                     &solutions);
+    print_solution(solutions,
+                   dimension_matrix);
     free(L);
-    free(LT);
+    free(U);
+    free(L_solutions);
+    free(solutions);
     return 0;
 }
