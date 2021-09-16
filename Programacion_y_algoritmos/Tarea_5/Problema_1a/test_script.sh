7c1a4f1c148534076ba42b84df9d2b6ba17dd393
giovannilopez9808 <giovannilopez9808@gmail.com>
2021-09-16 15:13:25 -0500

    Add test

---
+++ Programacion_y_algoritmos/Tarea_5/Problema_1a/test_script.sh
@@ -0,0 +1,10 @@
+make clean
+make 
+./main.out test_text.txt lorem
+./main.out test_text.txt sapien
+./main.out test_text.txt maecenas
+./main.out test_text.txt aliquam
+./main.out test_text.txt quam
+./main.out test_text.txt tor
+./main.out test_text.txt tortor
+
