5a9ce7a33407ddcc4944785ee44538da1d9962c9
giovannilopez9808 <giovannilopez9808@gmail.com>
2021-09-24 01:17:41 -0500

    Eigenvalues finish

--- Metodos_numericos/Tarea_09/print_results.h
+++ Metodos_numericos/Tarea_09/print_results.h
@@ -29,18 +29,17 @@ void print_matrix(double *matrix, int dimension_matrix[])
     }
     printf("\n");
 }
-void print_solution(double *solution, int *solution_pos, int dimension_matrix[])
+void print_solution(double *solution, int dimension_matrix[])
 {
     /* 
     Realiza la impresion de la solucion del sistema de ecuaciones
      */
-    sorted(solution_pos, solution, 0, dimension_matrix[0] - 1);
     print_lines();
     printf("Soluciones del sistema\n\n");
     for (int i = 0; i < dimension_matrix[0]; i++)
     {
         printf("x_%d\t= %lf\n",
-               *(solution_pos + i),
+               i,
                *(solution + i));
     }
 }
