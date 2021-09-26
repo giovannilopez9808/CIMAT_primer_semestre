91662334751d10cbba1db9fb5d366bdc5e09593a
giovannilopez9808 <giovannilopez9808@gmail.com>
2021-09-26 01:42:30 -0500

    Add print file

--- Programacion_y_algoritmos/Tarea_6/Script/main.c
+++ Programacion_y_algoritmos/Tarea_6/Script/main.c
@@ -10,18 +10,27 @@ int main(int argv, char *argc[])
     (void)argv;
     FILE *data = NULL;
     struct student *students;
-    open_file(&data, argc[1]);
+    open_file(&data,
+              argc[1]);
     int persons = obtain_size(data);
     int persons_per_group[6], persons_per_turn[2];
     (void)persons_per_group;
+    (void)persons_per_turn;
     students = (struct student *)calloc(persons, sizeof(struct student));
     obtain_information(students,
                        data,
                        persons);
+    //    Nombre
+    // sort(students, persons, 1);
+    // // Edades
+    // sort(students, persons, 2);
+    // // Calificacion
+    // sort(students, persons, 3);
     // count_per_group(students, persons, persons_per_group);
     // count_per_turn(students, persons, persons_per_turn);
     // print_person_per_group(persons_per_group);
     // print_person_per_turn(persons_per_turn);
+    // print_file(data, argc[1], students, persons);
     free(students);
     fclose(data);
     return 0;
--- Programacion_y_algoritmos/Tarea_6/Script/print_results.h
+++ Programacion_y_algoritmos/Tarea_6/Script/print_results.h
@@ -12,3 +12,22 @@ void print_person_per_turn(int persons[2])
     printf("Matutino\t%d\n", persons[0]);
     printf("Vespertino\t%d\n", persons[1]);
 }
+void print_file(FILE *data, char *filename, struct student *students, int size)
+{
+    fclose(data);
+    data = fopen(filename, "w");
+    fprintf(data, "Calificacion Edad Grupo Turno Nombre\n");
+    for (int i = 0; i < size; i++)
+    {
+        fprintf(data, "%s %d %c %c %s",
+                students[i].grade,
+                students[i].age,
+                students[i].s.group,
+                students[i].s.turn,
+                students[i].name);
+        if (i != size - 1)
+        {
+            fprintf(data, "\n");
+        }
+    }
+}
