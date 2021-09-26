e8c059af0c3ff5d029525e2771aa0ad76c902e7e
giovannilopez9808 <giovannilopez9808@gmail.com>
2021-09-26 02:14:13 -0500

    Simply update

--- Programacion_y_algoritmos/Tarea_6/Script/update.h
+++ Programacion_y_algoritmos/Tarea_6/Script/update.h
@@ -1,8 +1,8 @@
-void delete_user(struct student *students, int *size, int index)
+void delete_student(struct student *students, int *size, int index)
 {
     for (int i = index; i < *size - 1; i++)
     {
-        memcpy(&students[i], &students[i + 1], sizeof(struct student));
+        students[i] = students[i + 1];
     }
     *size = *size - 1;
 }
