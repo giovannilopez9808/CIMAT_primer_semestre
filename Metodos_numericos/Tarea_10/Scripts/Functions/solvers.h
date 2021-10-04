92c4cb2b0569aed6b3d5df0d2660bf080f8f7142
giovannilopez9808 <giovannilopez9808@gmail.com>
2021-10-04 12:21:02 -0500

    Add deflection inverse

---
+++ Metodos_numericos/Tarea_10/Scripts/Functions/solvers.h
@@ -0,0 +1,44 @@
+#ifndef SOLVERS_H
+#define SOLVERS_H
+#include <stdlib.h>
+#include <stdio.h>
+#include <math.h>
+#include "tools.h"
+/* 
+    Copia los valores del vector en el vector i
+     */
+void copy_vector(double *vector, double *vector_i, int dimension[]);
+/* 
+    Inicializa el vector dado con el valor 1/sqrt(n) en todos sus elementos
+     */
+void initialize_vector(double *vector, int dimension[]);
+/* 
+    Compara los valores de lambda para ver si se llega a una convergencia del metodo
+     */
+int convergence_eigenvalues(double lambda, double lambda_i, int attempt);
+/* 
+    Metodo de las potencias, recibe una matriz y su dimension y devuelve su eigenvalor dominante con su respectito eigenvector
+     */
+void obtain_max_eigenvalue(double *matrix, int dimension_matrix[], double *lambda, double **vector);
+/* 
+    Copia del vector i en la matriz de eigenvectores
+     */
+void copy_vector_i(double *vectors, double *vector, int dimension[], int n);
+/* 
+    Eliminacion de las contribuciones de los eigenvalores anteriores a el nuevo vector
+     */
+void obatin_new_vector(double **vector, double *vectors, int dimension[], int n);
+/* 
+    Copia del vector en la matriz de vectores unitarios
+     */
+void copy_vectors(double **vectors, double *vector, int dimension[], int n);
+/* 
+Metodo de deflaccion, dada una matriz y su dimension se calcularan los n lambdas dadas maximas
+*/
+void obtain_n_max_eigenvalue(double *matrix, int dimension_matrix[], double **lambda, double **vectors, int n_lambdas);
+/* 
+dada una matrix y su tamaño obtiene el eigenvalor menor absoluto y su respectivo eigenvector
+*/
+void obtain_min_eigenvalue(double *matrix, int dimension_matrix[], double *lambda, double **vector);
+void obtain_n_min_eigenvalue(double *matrix, int dimension_matrix[], double **lambda, double **vectors, int n_lambdas);
+#endif
---
+++ Metodos_numericos/Tarea_10/Scripts/Functions/tools.h
@@ -0,0 +1,130 @@
+#ifndef TOOLS_H
+#define TOOLS_H
+#include <stdlib.h>
+#include <stdio.h>
+#include <math.h>
+/* 
+    Realiza la impresion de las lineas en la terminal
+     */
+void print_lines();
+/* 
+    Realiza la impresion de las lineas en el documento
+     */
+void print_lines_on_file(FILE *text);
+/*
+    Realiza la validacion del sistema de ecuaciones lineales
+     */
+void valid_solution(double number);
+/*
+    Validacion del archivo
+     */
+void valid_file(FILE *text_file);
+/*
+    Lectura de las dimensiones de la matriz a partir de un archivo dado
+    inputs:
+    + file: puntero del archivo
+    + dimension: arreglo de dimension 2 de tipo entero
+     */
+void read_dimension(FILE *file, int dimension[]);
+/*
+    Lectura de los datos de la matriz a partir de un archivo dado
+    inputs:
+    + file: puntero del archivo
+    + dimension: arreglo de dimension 2 de tipo entero
+    + matrix: doble puntero de un tipo de dato double donde se alojaran los datos de la matriz
+     */
+void read_matrix(FILE *file, int dimension[], double **matrix);
+/*
+    Realiza la multiplicacion de matrices
+     */
+void obtain_multiplication_matrix(double *A, double *B, double *AB, int dimension_matrix_A[], int dimension_matrix_B[]);
+/* 
+    Calcula la norma de un vector dado
+     */
+double obtain_norm(double *vector, int dimension[]);
+/* 
+    Realiza la normalizacion del vector dado
+     */
+void normalize_vector(double *vector, int dimension[]);
+/* 
+    Realiza la multiplicacion de un vector por su transpuesta, dando como
+    restultado una matriz de nxn
+     */
+void obtain_multiplication_vvT(double *vector, int dimension[], double **matrix);
+/* 
+    Realiza el producto punto de dos vectores dados
+     */
+double obtain_cdot_multiplication(double *vector1, double *vector2, int dimension[]);
+/* 
+    Realiza la impresion en un archivo de un vector en una sola fila
+     */
+void print_eigenvector_on_a_file(FILE *text, double *vectors, int *dimension);
+/* 
+    Realiza la impresion de el eigenvalor dado en un archivo
+     */
+void print_lambda_on_a_file(FILE *text, double lambda);
+/* 
+    Realiza la impresion de los resultados de un eigenvalor y un eigen vector
+     */
+void print_results(FILE *text, double lambda, double *vectors, int *dimension);
+/* 
+    Realiza la impresion de un conjunto de eigenvalores y eigenvectores
+     */
+void print_several_results(FILE *text, double *lambda, double *vectors, int *dimension, int n);
+/*
+    Inicializa la matrix U con unos en la diagonal
+    inputs:
+    + U: puntero del arreglo de la matriz U
+    + dimension_matrix: arreglo de dos dimensiones que contiene el tamaño de la matriz
+     */
+void Fill_initial_values_U_matrix(double *U, int dimension_matrix[]);
+/*
+    Valida que la diagonal de la matriz L es diferente de cero
+     */
+void validate_l_ii(double l_ii);
+/*
+    Aplica la factorizacion LU aplicando la version de Crout
+    inputs:
+    + matrix: puntero del arreglo que contiene lo datos de la matriz
+    + dimension_matrix: puntero del arreglo de dos dimensiones que contiene el tamaño de la matriz
+    + L: doble puntero que contendra la matriz L
+    + U: doble puntero que contendra la matriz U
+     */
+void obtain_LU_crout(double *matrix, int dimension_matrix[], double **L, double **U);
+/*
+    Resuleve un sistema de ecuaciones supongiendo que se trata de una matriz triangular superior
+    inputs:
+    + matrix: puntero de un arreglo de datos bidimensional
+    + dimension_matrix: arreglo de enteros de dimension 2
+    + results: puntero de un arreglo que contiene los datos de la matriz extendida del sistema
+    + solutions: doble puntero de tipo double que alojara las soluciones del sistema
+     */
+void solve_triangular_superior_matrix(double *matrix, int dimension_matrix[], double *results, double **solutions);
+/*
+    Resuleve un sistema de ecuaciones supongiendo que se trata de una matriz triangular inferior
+    inputs:
+    + matrix: puntero de un arreglo de datos bidimensional
+    + dimension_matrix: arreglo de enteros de dimension 2
+    + results: puntero de un arreglo que contiene los datos de la matriz extendida del sistema
+    + solutions: doble puntero de tipo double que alojara las soluciones del sistema
+     */
+void solve_triangular_inferior_matrix(double *matrix, int dimension_matrix[], double *results, double **solutions);
+/*
+    Resuleve un sistema de ecuaciones supongiendo que se trata de una matriz diagonal
+    inputs:
+    + matrix: puntero de un arreglo de datos bidimensional
+    + dimension_matrix: arreglo de enteros de dimension 2
+    + results: puntero de un arreglo que contiene los datos de la matriz extendida del sistema
+    + solutions: doble puntero de tipo double que alojara las soluciones del sistema
+     */
+void solve_diagonal_matrix(double *matrix, int dimension_matrix[], double *results, double **solutions);
+/*
+    Resuleve un sistema de ecuaciones supongiendo que se trata de una matriz triangular superior
+    inputs:
+    + matrix: puntero de un arreglo de datos bidimensional
+    + dimension_matrix: arreglo de enteros de dimension 2
+    + results: puntero de un arreglo que contiene los datos de la matriz extendida del sistema
+    + solutions: doble puntero de tipo double que alojara las soluciones del sistema
+     */
+void solve_gaussian_matrix(double *matrix, int dimension_matrix[], double *results, double **solutions);
+#endif
