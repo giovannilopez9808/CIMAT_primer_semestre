7b9614c583aeb92366b77936bc1c84434c1600d8
giovannilopez9808 <giovannilopez9808@gmail.com>
2021-10-07 12:52:26 -0500

    Add Document

---
+++ Programacion_y_algoritmos/Tarea_07/Scripts/functions.h
@@ -0,0 +1,4 @@
+#include<stdio.h>
+#include<stdlib.h>
+void print_yes();
+FILE *read_wav(char *filename);
---
+++ Programacion_y_algoritmos/Tarea_07/Scripts/main.c
@@ -0,0 +1,6 @@
+#include "functions.h"
+int main()
+{
+    read_wav("ftea_10k.wav");
+    return 0;
+}
