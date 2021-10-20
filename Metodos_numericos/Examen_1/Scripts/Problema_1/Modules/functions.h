ff4c376754eb5d8dfe851d3dd3e373ce2fa3e3c0
Giovanni Lopez <giovannilopez9808@gmail.com>
2021-10-20 15:51:11 -0500

    Add Problem 3 y 4 skeleton

--- Metodos_numericos/Examen_1/Scripts/Problema_1/Modules/functions.h
+++
@@ -1,6 +0,0 @@
-#ifndef functions_H
-#define functions_H
-double obtain_h_derivative(double (*f)(double), double x);
-double obtain_derivative(double (*f)(double), double x);
-double h_function(double (*f)(double), double x);
-#endif
--- Metodos_numericos/Examen_1/Scripts/Problema_1/Modules/newton.h
+++
@@ -1,7 +0,0 @@
-#ifndef newton_H
-#define newton_H
-#include <math.h>
-#include <stdio.h>
-#include "functions.h"
-void newton_method(double (*f)(double), double tol_x, double tol_f, double x0);
-#endif
