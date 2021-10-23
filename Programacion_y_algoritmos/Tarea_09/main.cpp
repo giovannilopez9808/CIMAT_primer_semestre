bc0188091e96f7a55426a453f9e674c75352e3a7
Giovanni Lopez <giovannilopez9808@gmail.com>
2021-10-23 13:42:48 -0500

    Add tarea 9

---
+++ Programacion_y_algoritmos/Tarea_09/main.cpp
@@ -0,0 +1,19 @@
+/*
+ * Compilacion
+ * g++ main.cpp -o main.out
+ */
+#include <iostream>
+using namespace std;
+  
+int main()
+{ 
+    // Declararion 
+    double num; 
+    // Entrada
+    cout << "Ingresa un numero: ";
+    cin >> num;
+    // Impresion
+    cout << "El numero ingresado es: " << num;
+    cout << "\n";
+    return 0;
+}
