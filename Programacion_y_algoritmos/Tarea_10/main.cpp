576e8bc76412cc8fbf7cd82d04dfdd32ff8a5475
Giovanni Lopez <giovannilopez9808@gmail.com>
2021-10-31 17:33:36 -0600

    Add another case

--- Programacion_y_algoritmos/Tarea_10/main.cpp
+++ Programacion_y_algoritmos/Tarea_10/main.cpp
@@ -4,17 +4,21 @@ using namespace std;
 int main()
 {
     // Empleado usando estructuras
-    employee_s employee1;
+    employee_s employee_s_1, employee_s_2;
     // Empleado usando clases
-    employee_c employee2;
-    char name[40] = "giovanni";
+    employee_c employee_c_1, employee_c_2;
+    char name_1[40] = "Giovanni", name_2[40] = "Gamaliel";
     // Arquitectura usando estructuras
     cout << "Impreson usando estructuras" << endl;
-    init_employee(&employee1, 20, name);
-    print_information(employee1);
+    init_employee(&employee_s_1, 20, name_1);
+    print_information(employee_s_1);
+    init_employee(&employee_s_2, 16, name_2);
+    print_information(employee_s_2);
     // Arquitectura usando clases
     cout << "\nImpreson usando clases" << endl;
-    employee2.init_employee(20, name);
-    employee2.print_information();
+    employee_c_1.init_employee(20, name_1);
+    employee_c_1.print_information();
+    employee_c_2.init_employee(16, name_2);
+    employee_c_2.print_information();
     return 0;
 }
