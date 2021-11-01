5aabc005be40f17083b60aa68423198522245d19
giovannilopez9808 <giovannilopez9808@gmail.com>
2021-11-01 04:03:19 -0600

    Change colors and add input poitns

--- Metodos_numericos/Tarea_12/Scripts/Problema_04/graphic.py
+++ Metodos_numericos/Tarea_12/Scripts/Problema_04/graphic.py
@@ -6,23 +6,36 @@ def f(x):
     return x/(1+x**2)
 
 
+def create_points():
+    x = np.array([(i-8)/2 for i in range(17)])
+    y = f(x)
+    return x, y
+
+
 x, y = np.loadtxt("output.txt",
                   unpack=True)
-y_data = f(x)
+x_function = np.linspace(-4, 4, 1000)
+y_function = f(x_function)
+x_input, y_input = create_points()
 plt.xlim(-4, 4)
 plt.ylim(-0.6, 0.6)
+plt.plot(x_input, y_input,
+         label="Input points",
+         color="#007200",
+         ls="",
+         marker=".")
 plt.plot(x, y,
-         color="#370617",
+         color="#c1121f",
          label="Spline cubic",
          ls="--",
          marker=".")
-plt.plot(x, y_data,
-         color="#f48c06",
+plt.plot(x_function, y_function,
+         color="#70e000",
          label="$f\;(x)=\\dfrac{x}{1+x^2}$",
-         marker=".",
          alpha=0.5)
 plt.xticks(fontsize=12)
 plt.yticks(fontsize=12)
+
 plt.legend(frameon=False,
            fontsize=14)
 plt.tight_layout()
