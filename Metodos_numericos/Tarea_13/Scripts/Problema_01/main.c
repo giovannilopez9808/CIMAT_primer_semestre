cbec748152547b40c048a01489b3854ca6491811
Giovanni Lopez <giovannilopez9808@gmail.com>
2021-11-12 00:11:13 -0600

    Add problema 1

--- Metodos_numericos/Tarea_13/Scripts/Problema_01/main.c
+++ Metodos_numericos/Tarea_13/Scripts/Problema_01/main.c
@@ -12,22 +12,19 @@ double f2(double x)
 int main()
 {
     int n_list[5] = {3, 6, 9, 12, 15};
-    double result, a = -1.0, b = 1.0;
-    FILE *output = open_file("Output/f1.csv", "w");
-    fprintf(output, "n,result\n");
+    double result_1, result_2, a = -1.0, b = 1.0;
+    FILE *output_1 = open_file("Output/f1.csv", "w");
+    FILE *output_2 = open_file("Output/f2.csv", "w");
+    fprintf(output_1, "n,result\n");
+    fprintf(output_2, "n,result\n");
     for (int i = 0; i < 5; i++)
     {
-        result = simpson_method(f1, a, b, n_list[i]);
-        write_results(output, n_list[i], result);
+        result_1 = simpson_method(f1, a, b, n_list[i]);
+        result_2 = simpson_method(f2, a, b, n_list[i]);
+        write_results(output_1, n_list[i], result_1);
+        write_results(output_2, n_list[i], result_2);
     }
-    fclose(output);
-    output = open_file("Output/f2.csv", "w");
-    fprintf(output, "n,result\n");
-    for (int i = 0; i < 5; i++)
-    {
-        result = simpson_method(f2, a, b, n_list[i]);
-        write_results(output, n_list[i], result);
-    }
-    fclose(output);
+    fclose(output_1);
+    fclose(output_2);
     return 0;
 }
