dca44c213bf52780c9040c254be9b03f43737f4d
giovannilopez9808 <giovannilopez9808@gmail.com>
2021-11-12 01:25:24 -0600

    Add graphics

--- Metodos_numericos/Tarea_13/Scripts/Problema_02/graphic.py
+++ Metodos_numericos/Tarea_13/Scripts/Problema_02/graphic.py
@@ -1,4 +1,5 @@
 import matplotlib.pyplot as plt
+from pylab import *
 import numpy as np
 
 
@@ -11,17 +12,33 @@ def pn(x, n):
 
 
 x = np.linspace(-1, 1, 1000)
-for i in range(2, 6):
+plt.subplots(figsize=(8, 3))
+colors = ["#7400b8",
+          "#5390d9",
+          "#6a040f",
+          "#e85d04",
+          "#ffba08",
+          "#40916c",
+          "#72efdd",
+          "#f15bb5"]
+for i in range(2, 8):
     roots = np.loadtxt("Output/{}.csv".format(i),
                        usecols=2,
                        skiprows=1,
                        delimiter=",")
     y = [pn(xi, i) for xi in x]
     plt.plot(x, y,
-             label="$P_{}(x)$".format(i))
+             label="$P_{}(x)$".format(i),
+             color=colors[i-2])
     for root in roots:
-        plt.scatter(root, 0, c="#000000")
+        plt.scatter(root, 0, c=colors[i-2])
+plt.plot([-1, 1], [0, 0],
+         color="#000000",
+         ls="--")
+plt.ylim(-1, 1)
+plt.xlim(-1, 1)
+plt.tight_layout()
 plt.legend(frameon=False,
-           ncol=2)
+           ncol=3)
 plt.savefig("../../Document/Graphics/problema2.png",
             dpi=400)
