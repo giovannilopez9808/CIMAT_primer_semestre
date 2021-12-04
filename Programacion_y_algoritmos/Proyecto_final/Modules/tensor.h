41fc7f65ccb82e38694e57be8544d94a27c89d8b
Giovanni Lopez <giovannilopez9808@gmail.com>
2021-12-04 14:57:33 -0600

    Proyecto terminado

--- Programacion_y_algoritmos/Proyecto_final/Modules/tensor.h
+++ Programacion_y_algoritmos/Proyecto_final/Modules/tensor.h
@@ -6,85 +6,87 @@ template <typename type>
 class Tensor
 {
 private:
-    std::shared_ptr<type[]> m_data;
-    size_t m_ranks;
-    size_t m_cols;
-    size_t m_rows;
+    std::shared_ptr<type[]> data;
+    size_t ranks;
+    size_t cols;
+    size_t rows;
 
-    Tensor<type> *sum(std::shared_ptr<type[]> a, std::shared_ptr<type[]> b);
-    Tensor<type> *diff(std::shared_ptr<type[]> a, std::shared_ptr<type[]> b);
-    Tensor<type> *mult(std::shared_ptr<type[]> a, std::shared_ptr<type[]> b);
-    Tensor<type> *div(std::shared_ptr<type[]> a, std::shared_ptr<type[]> b);
+    Tensor<type> sum(std::shared_ptr<type[]> a, std::shared_ptr<type[]> b);
+    Tensor<type> diff(std::shared_ptr<type[]> a, std::shared_ptr<type[]> b);
+    Tensor<type> mult(std::shared_ptr<type[]> a, std::shared_ptr<type[]> b);
+    Tensor<type> div(std::shared_ptr<type[]> a, std::shared_ptr<type[]> b);
 
 public:
     Tensor();
-    Tensor(type *data,
-           size_t ranks,
-           size_t cols,
-           size_t rows);
-    Tensor(size_t ranks,
-           size_t cols,
-           size_t rows);
-    Tensor(size_t cols,
-           size_t rows);
+    Tensor(type *data_input,
+           size_t ranks_input,
+           size_t cols_input,
+           size_t rows_input);
+    Tensor(size_t ranks_input,
+           size_t cols_input,
+           size_t rows_input);
+    Tensor(size_t cols_input,
+           size_t row_inputs);
     Tensor(const Tensor &copy);
-    Tensor<type> *operator+(const Tensor<type> &operand);
-    Tensor<type> *operator*(const Tensor<type> &operand);
-    Tensor<type> *operator-(const Tensor<type> &operand);
-    Tensor<type> *operator/(const Tensor<type> &operand);
+    Tensor<type> operator+(const Tensor<type> &operand);
+    Tensor<type> operator*(const Tensor<type> &operand);
+    Tensor<type> operator-(const Tensor<type> &operand);
+    Tensor<type> operator/(const Tensor<type> &operand);
     size_t get_ranks();
     size_t get_cols();
     size_t get_rows();
     type *get_data();
+    void print();
     ~Tensor();
 };
 
 template <typename type>
-Tensor<type> *Tensor<type>::operator+(const Tensor<type> &operand)
+Tensor<type> Tensor<type>::operator+(const Tensor<type> &operand)
 {
-    return sum(m_data, operand.m_data);
+    return sum(data, operand.data);
 }
 template <typename type>
-Tensor<type> *Tensor<type>::operator*(const Tensor<type> &operand)
+Tensor<type> Tensor<type>::operator*(const Tensor<type> &operand)
 {
-    return mult(m_data, operand.m_data);
+    return mult(data, operand.data);
 }
 template <typename type>
-Tensor<type> *Tensor<type>::operator-(const Tensor<type> &operand)
+Tensor<type> Tensor<type>::operator-(const Tensor<type> &operand)
 {
-    return diff(m_data, operand.m_data);
+    return diff(data, operand.data);
 }
 template <typename type>
-Tensor<type> *Tensor<type>::operator/(const Tensor<type> &operand)
+Tensor<type> Tensor<type>::operator/(const Tensor<type> &operand)
 {
-    return div(m_data, operand.m_data);
+    return div(data, operand.data);
 }
 template <typename type>
 type *Tensor<type>::get_data()
 {
-    return m_data.get();
+    return data.get();
 }
 template <typename type>
-Tensor<type> *Tensor<type>::sum(std::shared_ptr<type[]> a, std::shared_ptr<type[]> b)
+Tensor<type> Tensor<type>::sum(std::shared_ptr<type[]> a, std::shared_ptr<type[]> b)
 {
     const auto pa = a.get();
     const auto pb = b.get();
-    Tensor<type> tensor(m_ranks, m_cols, m_rows);
-    const auto result = tensor.m_data.get();
-    for (int n = 0; n < m_ranks * m_cols * m_rows; n++)
+    // std::cout << ranks << rows << cols << std::endl;
+    Tensor<type> tensor(ranks, cols, rows);
+    const auto result = tensor.data.get();
+    for (int n = 0; n < ranks * cols * rows; n++)
     {
         result[n] = pa[n] + pb[n];
     }
     return tensor;
 }
 template <typename type>
-Tensor<type> *Tensor<type>::diff(std::shared_ptr<type[]> a, std::shared_ptr<type[]> b)
+Tensor<type> Tensor<type>::diff(std::shared_ptr<type[]> a, std::shared_ptr<type[]> b)
 {
     const auto pa = a.get();
     const auto pb = b.get();
-    Tensor<type> tensor(m_ranks, m_cols, m_rows);
-    const auto result = tensor.m_data.get();
-    for (int n = 0; n < m_ranks * m_cols * m_rows; n++)
+    Tensor<type> tensor(ranks, cols, rows);
+    const auto result = tensor.data.get();
+    for (size_t n = 0; n < ranks * cols * rows; n++)
     {
         result[n] = pa[n] - pb[n];
     }
@@ -92,13 +94,13 @@ Tensor<type> *Tensor<type>::diff(std::shared_ptr<type[]> a, std::shared_ptr<type
 }
 
 template <typename type>
-Tensor<type> *Tensor<type>::mult(std::shared_ptr<type[]> a, std::shared_ptr<type[]> b)
+Tensor<type> Tensor<type>::mult(std::shared_ptr<type[]> a, std::shared_ptr<type[]> b)
 {
     const auto pa = a.get();
     const auto pb = b.get();
-    Tensor<type> tensor(m_ranks, m_cols, m_rows);
-    const auto result = tensor.m_data.get();
-    for (int n = 0; n < m_ranks * m_cols * m_rows; n++)
+    Tensor<type> tensor(ranks, cols, rows);
+    const auto result = tensor.data.get();
+    for (size_t n = 0; n < ranks * cols * rows; n++)
     {
         result[n] = pa[n] * pb[n];
     }
@@ -106,13 +108,13 @@ Tensor<type> *Tensor<type>::mult(std::shared_ptr<type[]> a, std::shared_ptr<type
 }
 
 template <typename type>
-Tensor<type> *Tensor<type>::div(std::shared_ptr<type[]> a, std::shared_ptr<type[]> b)
+Tensor<type> Tensor<type>::div(std::shared_ptr<type[]> a, std::shared_ptr<type[]> b)
 {
     const auto pa = a.get();
     const auto pb = b.get();
-    Tensor<type> tensor(m_ranks, m_cols, m_rows);
-    const auto result = tensor.m_data.get();
-    for (int n = 0; n < m_ranks * m_cols * m_rows; n++)
+    Tensor<type> tensor(ranks, cols, rows);
+    const auto result = tensor.data.get();
+    for (size_t n = 0; n < ranks * cols * rows; n++)
     {
         result[n] = pa[n] / pb[n];
     }
@@ -121,48 +123,52 @@ Tensor<type> *Tensor<type>::div(std::shared_ptr<type[]> a, std::shared_ptr<type[
 template <typename type>
 Tensor<type>::Tensor()
 {
-    m_ranks = m_cols = m_rows = 0;
+    ranks = cols = rows = 0;
 }
 template <typename type>
 Tensor<type>::Tensor(
-    size_t cols,
-    size_t rows)
+    size_t cols_input,
+    size_t rows_input)
 {
-    m_ranks = 1;
-    m_cols = cols;
-    m_rows = rows;
-    m_data = std::make_shared<type>(new Tensor[m_ranks * m_rows * m_cols]);
+    ranks = 1;
+    cols = cols_input;
+    rows = rows_input;
+    data = std::shared_ptr<type[]>(new type[ranks * rows * cols]);
 }
 template <typename type>
 Tensor<type>::Tensor(
-    size_t ranks,
-    size_t cols,
-    size_t rows)
+    size_t ranks_input,
+    size_t cols_input,
+    size_t rows_input)
 {
-    m_ranks = ranks;
-    m_cols = cols;
-    m_rows = rows;
-    m_data = std::make_shared<type>(new Tensor[m_ranks * m_rows * m_cols]);
+    ranks = ranks_input;
+    cols = cols_input;
+    rows = rows_input;
+    data = std::shared_ptr<type[]>(new type[cols * rows * ranks]);
 }
 template <typename type>
-Tensor<type>::Tensor(type *data,
-                     size_t ranks,
-                     size_t cols,
-                     size_t rows)
+Tensor<type>::Tensor(type *data_input,
+                     size_t ranks_input,
+                     size_t cols_input,
+                     size_t rows_input)
 {
-    m_ranks = ranks;
-    m_cols = cols;
-    m_rows = rows;
-    m_data = std::shared_ptr<type[]>(data);
+    ranks = ranks_input;
+    cols = cols_input;
+    rows = rows_input;
+    data = std::shared_ptr<type[]>(data_input);
 }
 template <typename type>
 Tensor<type>::Tensor(
     const Tensor &copy)
 {
-    m_ranks = copy.m_ranks;
-    m_cols = copy.m_cols;
-    m_rows = copy.m_rows;
-    m_data = copy.m_data;
+    ranks = copy.ranks;
+    cols = copy.cols;
+    rows = copy.rows;
+    data = std::shared_ptr<type[]>(new type[cols * rows * ranks]);
+    for (size_t i = 0; i < cols * rows * ranks; i++)
+    {
+        data[i] = copy.data[i];
+    }
 }
 template <typename type>
 Tensor<type>::~Tensor()
@@ -171,16 +177,33 @@ Tensor<type>::~Tensor()
 template <typename type>
 size_t Tensor<type>::get_ranks()
 {
-    return m_ranks;
+    return ranks;
 }
 template <typename type>
 size_t Tensor<type>::get_cols()
 {
-    return m_cols;
+    return cols;
 }
 template <typename type>
 size_t Tensor<type>::get_rows()
 {
-    return m_rows;
+    return rows;
+}
+template <typename type>
+void Tensor<type>::print()
+{
+    for (size_t i = 0; i < ranks; i++)
+    {
+        std::cout << "Capa " << i + 1 << std::endl;
+        for (size_t j = 0; j < rows; j++)
+        {
+            for (size_t k = 0; k < cols; k++)
+            {
+                std::cout << data[i * (rows * cols) + j * cols + k] << " ";
+            }
+            std::cout << std::endl;
+        }
+        std::cout << std::endl;
+    }
 }
 #endif
--- Programacion_y_algoritmos/Proyecto_final/main.cpp
+++ Programacion_y_algoritmos/Proyecto_final/main.cpp
@@ -3,22 +3,30 @@
 
 int main()
 {
-    size_t cols = 10;
-    size_t rows = 20;
-    size_t ranks = 30;
+    size_t cols = 3;
+    size_t rows = 4;
+    size_t ranks = 2;
     double *t = new double[cols * rows * ranks];
-    for (int i = 0; i < cols * rows * ranks; i++)
+    for (size_t i = 0; i < cols * rows * ranks; i++)
     {
-        t[i]=i;
+        t[i] = i + 1;
     }
     Tensor<double> b(t, ranks, cols, rows);
     Tensor<double> c(b);
-    for (int i = 0; i < rows * cols * ranks; i++)
-    {
-        std::cout << i;
-        std::cout << " ";
-        std::cout << rows * cols * ranks;
-        std::cout << " ";
-        std::cout << c.get_data()[i] << std::endl;
-    }
+    Tensor<double> suma = b + c;
+    Tensor<double> resta = b - c;
+    Tensor<double> multi = b * c;
+    Tensor<double> division = b / c;
+    std::cout << "====== Matriz a ======" << std::endl;
+    b.print();
+    std::cout << "====== Matriz b ======" << std::endl;
+    c.print();
+    std::cout << "====== Suma ======" << std::endl;
+    suma.print();
+    std::cout << "====== Resta ======" << std::endl;
+    resta.print();
+    std::cout << "====== Multiplicacion ======" << std::endl;
+    multi.print();
+    std::cout << "====== Division ======" << std::endl;
+    division.print();
 }
