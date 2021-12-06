ba3b803e703b41e14d0f8bd88d3693cd9fcf3eac
giovannilopez9808 <giovannilopez9808@gmail.com>
2021-12-06 04:42:38 -0600

    Add documentation

--- Programacion_y_algoritmos/Proyecto_final/Modules/tensor.h
+++ Programacion_y_algoritmos/Proyecto_final/Modules/tensor.h
@@ -2,41 +2,67 @@
 #define tensor_H
 #include <iostream>
 #include <memory>
+// Definicion de la clase Tensor
 template <typename type>
 class Tensor
 {
 private:
+    // Valores del tensor
     std::shared_ptr<type[]> data;
+    // Numero de rango
     size_t ranks;
+    // Numero de columnas
     size_t cols;
+    // Numero de filas
     size_t rows;
-
+    // Definicion de la operacion suma
     Tensor<type> sum(std::shared_ptr<type[]> a, std::shared_ptr<type[]> b);
+
+    // Definicion de la operacion resta
     Tensor<type> diff(std::shared_ptr<type[]> a, std::shared_ptr<type[]> b);
+
+    // Definicion de la operacion multiplicacion
     Tensor<type> mult(std::shared_ptr<type[]> a, std::shared_ptr<type[]> b);
+
+    // Definicion de la operacion division
     Tensor<type> div(std::shared_ptr<type[]> a, std::shared_ptr<type[]> b);
 
 public:
+    // Constructor vacio
     Tensor();
+    // Constructor dado una lista y su tamaño
     Tensor(type *data_input,
            size_t ranks_input,
            size_t cols_input,
            size_t rows_input);
+    // Constructor dado el tamaño
     Tensor(size_t ranks_input,
            size_t cols_input,
            size_t rows_input);
+    // Constructor dado el tamaño como matriz
     Tensor(size_t cols_input,
            size_t row_inputs);
+    //    Constructor dado un tensor
     Tensor(const Tensor &copy);
+    // Definicion de la suma con el operador +
     Tensor<type> operator+(const Tensor<type> &operand);
+    // Definicion de la multiplicacion con el operador *
     Tensor<type> operator*(const Tensor<type> &operand);
+    // Definicion de la resta con el operador -
     Tensor<type> operator-(const Tensor<type> &operand);
+    // Definicion de la division con el operador /
     Tensor<type> operator/(const Tensor<type> &operand);
+    // Obtiene el numero de rango
     size_t get_ranks();
+    // Obtiene el numero de columnas
     size_t get_cols();
+    // Obtiene el numero de filas
     size_t get_rows();
+    // Obtiene los datos
     type *get_data();
+    // Imprime el tensor
     void print();
+    // Destructor
     ~Tensor();
 };
 
@@ -45,47 +71,60 @@ Tensor<type> Tensor<type>::operator+(const Tensor<type> &operand)
 {
     return sum(data, operand.data);
 }
+
 template <typename type>
 Tensor<type> Tensor<type>::operator*(const Tensor<type> &operand)
 {
     return mult(data, operand.data);
 }
+
 template <typename type>
 Tensor<type> Tensor<type>::operator-(const Tensor<type> &operand)
 {
     return diff(data, operand.data);
 }
+
 template <typename type>
 Tensor<type> Tensor<type>::operator/(const Tensor<type> &operand)
 {
     return div(data, operand.data);
 }
+
 template <typename type>
 type *Tensor<type>::get_data()
 {
     return data.get();
 }
+
 template <typename type>
 Tensor<type> Tensor<type>::sum(std::shared_ptr<type[]> a, std::shared_ptr<type[]> b)
 {
+    // Obtiene el valor del primer tensor
     const auto pa = a.get();
+    // Obtiene el valor del segundo tensor
     const auto pb = b.get();
-    // std::cout << ranks << rows << cols << std::endl;
+    // Creacion del tensor vacio
     Tensor<type> tensor(ranks, cols, rows);
     const auto result = tensor.data.get();
+    // Calculo de la suma
     for (int n = 0; n < ranks * cols * rows; n++)
     {
         result[n] = pa[n] + pb[n];
     }
     return tensor;
 }
+
 template <typename type>
 Tensor<type> Tensor<type>::diff(std::shared_ptr<type[]> a, std::shared_ptr<type[]> b)
 {
+    // Obtiene el valor del primer tensor
     const auto pa = a.get();
+    // Obtiene el valor del segundo tensor
     const auto pb = b.get();
+    // Creacion del tensor vacio
     Tensor<type> tensor(ranks, cols, rows);
     const auto result = tensor.data.get();
+    // Calculo de la resta
     for (size_t n = 0; n < ranks * cols * rows; n++)
     {
         result[n] = pa[n] - pb[n];
@@ -96,10 +135,14 @@ Tensor<type> Tensor<type>::diff(std::shared_ptr<type[]> a, std::shared_ptr<type[
 template <typename type>
 Tensor<type> Tensor<type>::mult(std::shared_ptr<type[]> a, std::shared_ptr<type[]> b)
 {
+    // Obtiene el valor del primer tensor
     const auto pa = a.get();
+    // Obtiene el valor del segundo tensor
     const auto pb = b.get();
+    // Creacion del tensor vacio
     Tensor<type> tensor(ranks, cols, rows);
     const auto result = tensor.data.get();
+    // Calculo de la multiplicacion
     for (size_t n = 0; n < ranks * cols * rows; n++)
     {
         result[n] = pa[n] * pb[n];
@@ -109,11 +152,16 @@ Tensor<type> Tensor<type>::mult(std::shared_ptr<type[]> a, std::shared_ptr<type[
 
 template <typename type>
 Tensor<type> Tensor<type>::div(std::shared_ptr<type[]> a, std::shared_ptr<type[]> b)
+/* D */
 {
+    // Obtiene el valor del primer tensor
     const auto pa = a.get();
+    // Obtiene el valor del segundo tensor
     const auto pb = b.get();
+    // Creacion del tensor vacio
     Tensor<type> tensor(ranks, cols, rows);
     const auto result = tensor.data.get();
+    // Calculo de la division
     for (size_t n = 0; n < ranks * cols * rows; n++)
     {
         result[n] = pa[n] / pb[n];
@@ -123,6 +171,7 @@ Tensor<type> Tensor<type>::div(std::shared_ptr<type[]> a, std::shared_ptr<type[]
 template <typename type>
 Tensor<type>::Tensor()
 {
+    // Creacion del tensor vacio
     ranks = cols = rows = 0;
 }
 template <typename type>
@@ -165,6 +214,7 @@ Tensor<type>::Tensor(
     cols = copy.cols;
     rows = copy.rows;
     data = std::shared_ptr<type[]>(new type[cols * rows * ranks]);
+    // Copia de los valores, el cambio en un tensor no afecta al otro
     for (size_t i = 0; i < cols * rows * ranks; i++)
     {
         data[i] = copy.data[i];
--- Programacion_y_algoritmos/Proyecto_final/main.cpp
+++ Programacion_y_algoritmos/Proyecto_final/main.cpp
@@ -1,25 +1,32 @@
-#include <iostream>
 #include "Modules/tensor.h"
 
 int main()
 {
+    // Definicion del tamaño del tensor
     size_t cols = 3;
     size_t rows = 4;
     size_t ranks = 2;
+    // Tensor raw creado
     double *t = new double[cols * rows * ranks];
     for (size_t i = 0; i < cols * rows * ranks; i++)
     {
         t[i] = i + 1;
     }
+    // Inicializacion del tensor a partir de una lista
     Tensor<double> b(t, ranks, cols, rows);
+    // Inicializacion de un tensor a partir de un tensor
     Tensor<double> c(b);
+    // Calculo de la suma
     Tensor<double> suma = b + c;
+    // Calculo de la resta
     Tensor<double> resta = b - c;
+    // Calculo de la multiplicacion
     Tensor<double> multi = b * c;
+    // Calculo de la division
     Tensor<double> division = b / c;
-    std::cout << "====== Matriz a ======" << std::endl;
+    std::cout << "====== Tensor a ======" << std::endl;
     b.print();
-    std::cout << "====== Matriz b ======" << std::endl;
+    std::cout << "====== Tensor b ======" << std::endl;
     c.print();
     std::cout << "====== Suma ======" << std::endl;
     suma.print();
