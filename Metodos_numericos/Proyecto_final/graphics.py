bb5bc4dc5fcba5577fb6c15e2d9dede8cd2b06bd
giovannilopez9808 <giovannilopez9808@gmail.com>
2021-12-08 05:54:24 -0600

    Change position

--- Metodos_numericos/Proyecto_final/graphics.py
+++ Metodos_numericos/Proyecto_final/graphics.py
@@ -1,6 +1,6 @@
 import matplotlib.pyplot as plt
 import numpy as np
-data = np.loadtxt("u.txt")
+data = np.loadtxt("Output/output.txt")
 data = np.transpose(data)
 time = np.linspace(0, 1, 101)
 plt.imshow(data,
