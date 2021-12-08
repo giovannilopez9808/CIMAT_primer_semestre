26dd6b269caaef4ceb317c91e487b754d5a6f538
giovannilopez9808 <giovannilopez9808@gmail.com>
2021-12-08 05:13:44 -0600

    Create valid_obtain_factorization

--- Metodos_numericos/Proyecto_final/Modules/heat_equation.c
+++ Metodos_numericos/Proyecto_final/Modules/heat_equation.c
@@ -1,92 +1,48 @@
 #include "heat_equation.h"
 /*
-  Purpose:
-    F returns the right hand side of the heat equation.
+  Crea el el vector del lado derecho de la ecuacion de calor
 
-  Parameters:
-    Input, double A, B, the left and right endpoints.
-    Input, double T0, the initial time.
-    Input, double T, the current time.
-    Input, int N, the number of point.
-    Input, double X[N], the current spatial positions.
-
-    Output, double VALUE[N], the prescribed value of U(X(:),T0).
+  Parametros:
+    Input, int n, numero de puntos.
+    Input, double* vector, posiciones.
 */
 void create_vector(double value, double *vector, int n)
 {
-  int i;
-
-  for (i = 1; i < n - 1; i++)
+  for (int i = 1; i < n - 1; i++)
   {
     vector[i] = value;
   }
 }
 
-/*
-  Purpose:
-
-    obtain_factorization factors a R83 system without pivoting.
-
-  Discussion:
-
-    The R83 storage format is used for a tridiagonal matrix.
-    The superdiagonal is stored in entries (1,2:N), the diagonal in
-    entries (2,1:N), and the subdiagonal in (3,1:N-1).  Thus, the
-    original matrix is "collapsed" vertically into the array.
-
-    Because this routine does not use pivoting, it can fail even when
-    the matrix is not singular, and it is liable to make larger
-    errors.
-
-    obtain_factorization and solve_matrix may be preferable to the corresponding
-    LINPACK routine SGTSL for tridiagonal systems, which factors and solves
-    in one step, and does not save the factorization.
-
-  Example:
-
-    Here is how a R83 matrix of order 5 would be stored:
-
-       *  A12 A23 A34 A45
-      A11 A22 A33 A44 A55
-      A21 A32 A43 A54  *
-  Parameters:
-
-    Input, int N, the order of the matrix.
-    N must be at least 2.
-
-    Input/output, double A[3*N].
-    On input, the tridiagonal matrix.  On output, factorization information.
-
-    Output, int obtain_factorization, singularity flag.
-    0, no singularity detected.
-    nonzero, the factorization failed on the INFO-th step.
-*/
-int obtain_factorization(int n, double *a)
-
+void valid_obtain_factorization(double *matrix, int i)
 {
-  for (int i = 1; i < n; i++)
-  {
-    if (a[1 + (i - 1) * 3] == 0.0)
-    {
-      printf("\n");
-      printf("obtain_factorization - Fatal error!\n");
-      printf("  Zero pivot on step %d\n", i);
-      return i;
-    }
-    // Store the multiplier in L.
-    a[2 + (i - 1) * 3] = a[2 + (i - 1) * 3] / a[1 + (i - 1) * 3];
-    // Modify the diagonal entry in the next column.
-    a[1 + i * 3] = a[1 + i * 3] - a[2 + (i - 1) * 3] * a[0 + i * 3];
-  }
-
-  if (a[1 + (n - 1) * 3] == 0.0)
+  if (matrix[1 + (i - 1) * 3] == 0.0)
   {
     printf("\n");
-    printf("obtain_factorization - Fatal error!\n");
-    printf("  Zero pivot on step %d\n", n);
-    return n;
+    printf("==== obtain_factorization - Error ====\n");
+    printf("====  Division entre cero %d ====\n", i);
+    exit(1);
   }
-  return 0;
+}
+/*
+  Realiza la factorizacion de la matriz tridimensional
+
+  Parametros:
+  Input: int dimension, dimension de la matriz
+  Input: double* matrix, matriz tridiagonal
+
+*/
+void obtain_factorization(int dimension, double *matrix)
+{
+  for (int i = 1; i < dimension; i++)
+  {
+    valid_obtain_factorization(matrix, i);
+    // Store the multiplier in L.
+    matrix[2 + (i - 1) * 3] = matrix[2 + (i - 1) * 3] / matrix[1 + (i - 1) * 3];
+    // Modify the diagonal entry in the next column.
+    matrix[1 + i * 3] = matrix[1 + i * 3] - matrix[2 + (i - 1) * 3] * matrix[0 + i * 3];
+  }
+  valid_obtain_factorization(matrix, dimension);
 }
 
 /*
@@ -99,7 +55,7 @@ int obtain_factorization(int n, double *a)
     entries (2,1:N), and the subdiagonal in (3,1:N-1).  Thus, the
     original matrix is "collapsed" vertically into the array.
 
-  Parameters:
+  Parametros:
     Input, int N, the order of the matrix.
     N must be at least 2.
     Input, double matrix[3*N], the LU factors from obtain_factorization.
@@ -139,7 +95,7 @@ double *solve_matrix(int n, double *matrix, double *vector)
   Purpose:
     set_initial_state returns the initial condition at the starting time.
 
-  Parameters:
+  Parametros:
 
     Input, double A, B, the left and right endpoints
     Input, double T0, the initial time.
@@ -148,11 +104,11 @@ double *solve_matrix(int n, double *matrix, double *vector)
     Input, double X[N], the positions where initial data is needed.
     Output, double VALUE[N], the prescribed value of U(X,T0).
 */
-void set_initial_state(Parameters parameters, double *vector, int n)
+void set_initial_state(Parameters Parametros, double *vector, int n)
 {
   for (int i = 0; i < n; i++)
   {
-    vector[i] =parameters.u0;
+    vector[i] = Parametros.u0;
   }
 }
 /*
@@ -215,38 +171,38 @@ double *define_A_matrix(double k, double t_delta, double x_delta, int x_num)
       =               dt             * F(X,   T+dt)
       +                                U(X,   T)
 */
-void solve_system(Parameters parameters)
+void solve_system(Parameters Parametros)
 {
   double *a, *b, *fvec, *x, *t, *u;
   int x_num, t_num;
   char *u_file = "u.txt";
   double k, x_max, x_min, x_delta, t_max, t_min, t_delta;
-  k = parameters.k;
+  k = Parametros.k;
   //   Set X values.
-  x_min = parameters.x_min;
-  x_max = parameters.x_max;
-  x_num = parameters.x_num;
+  x_min = Parametros.x_min;
+  x_max = Parametros.x_max;
+  x_num = Parametros.x_num;
   x_delta = (x_max - x_min) / (double)(x_num - 1);
   x = linspace(x_min, x_max, x_num);
   //   Set T values.
-  t_min = parameters.t_min;
-  t_max = parameters.t_max;
-  t_num = parameters.t_num;
+  t_min = Parametros.t_min;
+  t_max = Parametros.t_max;
+  t_num = Parametros.t_num;
   t_delta = (t_max - t_min) / (double)(t_num - 1);
   t = linspace(t_min, t_max, t_num);
   b = (double *)malloc(x_num * sizeof(double));
   fvec = (double *)malloc(x_num * sizeof(double));
   // Set the initial data, for time T_MIN.
   u = (double *)malloc(x_num * t_num * sizeof(double));
-  set_initial_state(parameters,u, x_num);
+  set_initial_state(Parametros, u, x_num);
   a = define_A_matrix(k, t_delta, x_delta, x_num);
   // Factor the matrix.
   obtain_factorization(x_num, a);
   for (int i = 1; i < t_num; i++)
   {
     //   Set the right hand side B.
-    b[0] = parameters.ua;
-    b[x_num - 1] = parameters.ub;
+    b[0] = Parametros.ua;
+    b[x_num - 1] = Parametros.ub;
     create_vector(1.0, fvec, x_num);
     for (int j = 1; j < x_num - 1; j++)
     {
