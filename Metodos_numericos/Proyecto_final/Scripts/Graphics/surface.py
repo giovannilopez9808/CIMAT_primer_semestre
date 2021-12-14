eba1b82aed494f3cd7f35c37e90e3530ad5f6a80
Giovanni Lopez <giovannilopez9808@gmail.com>
2021-12-14 01:59:33 -0600

    Add all datasets

--- Metodos_numericos/Proyecto_final/Scripts/Graphics/surface.py
+++ Metodos_numericos/Proyecto_final/Scripts/Graphics/surface.py
@@ -2,9 +2,9 @@ import matplotlib.pyplot as plt
 import numpy as np
 
 
-def same_plot(ax: plt.axes):
+def same_plot(ax: plt.axes, xlim: float):
     ax.view_init(26, 37)
-    ax.set_xlim(0, 1)
+    ax.set_xlim(0, xlim)
     ax.set_ylim(0, 1)
     ax.set_zlim(0, 1)
     ax.xaxis.set_pane_color((1.0, 1.0, 1.0, 0.0))
@@ -12,8 +12,8 @@ def same_plot(ax: plt.axes):
     ax.zaxis.set_pane_color((1.0, 1.0, 1.0, 0.0))
 
 
-def obtain_xy(data: np.array):
-    x = np.linspace(0, 1, np.shape(data)[0])
+def obtain_xy(data: np.array, xlim: float):
+    x = np.linspace(0, xlim, np.shape(data)[0])
     y = np.linspace(0, 1, np.shape(data)[1])
     return x, y
 
@@ -23,19 +23,47 @@ def f1(x: np.array, y: np.array):
     return z
 
 
+def f2(x: np.array, y: np.array):
+    z = np.exp(-y)*np.sin(np.pi*x)
+    return z
+
+
+def f3(x: np.array, y: np.array):
+    z = np.exp(-np.pi**2*y)*np.sin(np.pi*x)
+    return z
+
+
+def f4(x: np.array, y: np.array):
+    z = (np.exp(y)-np.exp(-4*y))*np.sin(x)/5
+    return z
+
+
 parameters = {"path data": "../Output/",
               "path graphics": "Output/",
-              "datasets": {"1": {
-                  "function": f1,
-                  "file data": "output.txt",
-                  "file graphics": "surface.png"}
+              "datasets": {
+                  "1": {"function": f1,
+                        "xlim": 1,
+                        "file data": "output_1.txt",
+                        "file graphics": "surface_1.png"},
+                  "2": {"function": f2,
+                        "xlim": 1,
+                        "file data": "output_2.txt",
+                        "file graphics": "surface_2.png"},
+                  "3": {"function": f3,
+                        "xlim": 1,
+                        "file data": "output_3.txt",
+                        "file graphics": "surface_3.png"},
+                  "4": {"function": f4,
+                        "xlim": np.pi,
+                        "file data": "output_4.txt",
+                        "file graphics": "surface_4.png"}
               }}
 
-dataset = parameters["datasets"]["1"]
+dataset = parameters["datasets"]["4"]
 f = dataset["function"]
 data = np.loadtxt("{}{}".format(parameters["path data"],
                                 dataset["file data"]))
-x, y = obtain_xy(data)
+x, y = obtain_xy(data, dataset["xlim"])
 x, y = np.meshgrid(x, y)
 z = f(x, y)
 fig, (ax1, ax2) = plt.subplots(1, 2,
@@ -45,8 +73,10 @@ surf = ax1.plot_surface(x, y, z,
                         cmap="inferno")
 ax2.plot_surface(x, y, data,
                  cmap="inferno")
-same_plot(ax1)
-same_plot(ax2)
+ax1.set_title("Solución análitica")
+ax2.set_title("Solución numérica")
+same_plot(ax1,dataset["xlim"])
+same_plot(ax2,dataset["xlim"])
 fig.colorbar(surf,
              ax=ax2,
              shrink=0.5,
--- Metodos_numericos/Proyecto_final/Scripts/main.c
+++ Metodos_numericos/Proyecto_final/Scripts/main.c
@@ -7,6 +7,21 @@ double f0_1(double x)
 {
     return sin(pi * x);
 }
+double f_2(double x, double t)
+{
+    (void)x;
+    (void)t;
+    return 0.0;
+}
+double f_4(double x, double t)
+{
+    return exp(t) * sin(x);
+}
+double f0_4(double x)
+{
+    (void)x;
+    return 0.0;
+}
 Parameters select_dataset(int select)
 {
     Parameters parameters;
@@ -24,7 +39,46 @@ Parameters select_dataset(int select)
         parameters.ub = 0;
         parameters.f0 = f0_1;
         parameters.f = f_1;
-
+        break;
+    case 2:
+        parameters.x_min = 0.0;
+        parameters.x_max = 1.0;
+        parameters.x_num = 20;
+        parameters.t_min = 0.0;
+        parameters.t_max = 1.0;
+        parameters.t_num = 20;
+        parameters.k = 1 / (pi * pi);
+        parameters.ua = 0;
+        parameters.ub = 0;
+        parameters.f0 = f0_1;
+        parameters.f = f_2;
+        break;
+    case 3:
+        parameters.x_min = 0.0;
+        parameters.x_max = 1.0;
+        parameters.x_num = 20;
+        parameters.t_min = 0.0;
+        parameters.t_max = 1.0;
+        parameters.t_num = 20;
+        parameters.k = 1;
+        parameters.ua = 0;
+        parameters.ub = 0;
+        parameters.f0 = f0_1;
+        parameters.f = f_2;
+        break;
+    case 4:
+        parameters.x_min = 0.0;
+        parameters.x_max = pi;
+        parameters.x_num = 100;
+        parameters.t_min = 0.0;
+        parameters.t_max = 1.0;
+        parameters.t_num = 100;
+        parameters.k = 4;
+        parameters.ua = 0;
+        parameters.ub = 0;
+        parameters.f0 = f0_4;
+        parameters.f = f_4;
+        break;
     default:
         break;
     }
@@ -32,7 +86,7 @@ Parameters select_dataset(int select)
 }
 int main()
 {
-    Parameters parameters = select_dataset(1);
+    Parameters parameters = select_dataset(4);
     solve_system(parameters);
     return 0;
 }
