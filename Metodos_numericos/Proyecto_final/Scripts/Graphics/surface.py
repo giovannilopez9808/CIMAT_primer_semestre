8454c2c290b253329158d562404c97fd63414413
Giovanni Lopez <giovannilopez9808@gmail.com>
2021-12-13 19:28:03 -0600

    Add surface plot

--- Metodos_numericos/Proyecto_final/Scripts/Graphics/surface.py
+++ Metodos_numericos/Proyecto_final/Scripts/Graphics/surface.py
@@ -1,11 +1,13 @@
 import matplotlib.pyplot as plt
 import numpy as np
 data = np.loadtxt("../Output/output.txt")
-data = np.transpose(data)
+# data = np.transpose(data)
 x = np.linspace(0, 1, np.shape(data)[0])
 y = np.linspace(0, 1, np.shape(data)[1])
 x, y = np.meshgrid(y, y)
-print(np.shape(x), np.shape(y))
+z = np.exp(-np.pi**2*y)*np.sin(np.pi*x) + x*(1 - x) * np.sin(10*y)
 fig, ax = plt.subplots(subplot_kw={"projection": "3d"})
-ax.plot_surface(y, x, data)
+ax.plot_surface(x, y, z)
+ax.plot_surface(x, y, data,
+                cmap="inferno")
 plt.show()
